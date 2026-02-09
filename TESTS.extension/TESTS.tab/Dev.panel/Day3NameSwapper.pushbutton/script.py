de2703a15fc6da2906b7433eee778ac56aa910a9
juliamlima96 <juliamlima96@gmail.com>
2026-02-09 17:46:57 +0000

    rename updates

--- TESTS.extension/TESTS.tab/Dev.panel/Day3NameSwapper.pushbutton/script.py
+++ TESTS.extension/TESTS.tab/Dev.panel/Day3NameSwapper.pushbutton/script.py
@@ -28,9 +28,9 @@ doc    = __revit__.ActiveUIDocument.Document #type:Document
 # ╩ ╩╩ ╩╩╝╚╝
 #==================================================
 
-#collector to get all views in the project and their names
+#collector to get all views in the project, and create a set of existing (ViewType, Name) combinations to check against when renaming views.
 all_views = FilteredElementCollector(doc).OfClass(View).ToElements()
-existing_names = set([v.Name for v in all_views])
+existing_keys = set([(v.ViewType, v.Name) for v in all_views])
 
 #forms to select views to be renamed
 selected_views = forms.select_views(
@@ -66,17 +66,56 @@ if not values['prefix'] and not values['find'] and not values['replace'] and not
     script.exit()
 
 #change names of selected views
+renaming_views = []
+duplicated_keys = set()
+temp_keys =set((v.ViewType, v.Name) for v in selected_views)
+
 for view in selected_views:
     old_name = view.Name
     base_name = old_name
 
     if Find and Find in old_name:
         base_name = old_name.replace(Find, Replace)
-    
+
     new_name = Prefix + base_name + Suffix
-    print (old_name, "==>", new_name)
+    old_key = (view.ViewType, old_name)
+    new_key = (view.ViewType, new_name)
 
-#check for duplicate names
+    #discard temporarily
+    temp_keys.discard(old_key)
+
+    if new_key in temp_keys:
+        duplicated_keys.add(new_key)
+        temp_keys.add(old_key) #undo discard of old key
+        continue
 
 
+    if (new_key in existing_keys) and (new_name != old_name):
+        duplicated_keys.add((old_key))
+        temp_keys.add(old_key) #undo discard of old key
+        continue
+
+    temp_keys.add(new_key)
+
+#temporary list (validation)
+#================================================
+#    renaming_views.append((view, old_name, new_name))
+#    print(old_name, "==>", new_name)
+#
+#print("Conflicts:")
+#for vt, nm in sorted(list(duplicated_keys), key=lambda x: (str(x[0]), x[1])):
+#    print(" - {} ({})".format(nm, vt))
 #==================================================
+
+#Apply renaming in Revit
+if not renaming_views:
+    forms.alert("No views renamed. Please check for naming conflicts.")
+    script.exit()  
+
+failed = []
+
+t = Transaction(doc, "Rename Views")
+t.Start()
+
+print ("Renaming Views...")
+for view in selected_views
